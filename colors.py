7ba72cee349c28eec2649583286de6abfd38cb36
Frankium <tis7bfrankie@gmail.comm>
2020-05-08 17:27:41 +0800

    fix foreground color

--- colors.py
+++ colors.py
@@ -10,14 +10,14 @@ FGColors = {
 	"m":formatHeader.format("35"),
 	"c":formatHeader.format("36"),
 	"w":formatHeader.format("37"),
-	"K":formatHeader.format("30;0"),
-	"R":formatHeader.format("31;0"),
-	"G":formatHeader.format("32;0"),
-	"Y":formatHeader.format("33;0"),
-	"B":formatHeader.format("34;0"),
-	"M":formatHeader.format("35;0"),
-	"C":formatHeader.format("36;0"),
-	"W":formatHeader.format("37;0")}
+	"K":formatHeader.format("30;1"),
+	"R":formatHeader.format("31;1"),
+	"G":formatHeader.format("32;1"),
+	"Y":formatHeader.format("33;1"),
+	"B":formatHeader.format("34;1"),
+	"M":formatHeader.format("35;1"),
+	"C":formatHeader.format("36;1"),
+	"W":formatHeader.format("37;1")}
 
 BGColors={
 	"k":formatHeader.format("40"),
