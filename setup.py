8ee00c2ae374c2fd54ec8fef6fa216ec66e3a5ba
mclt0568 <tis7bfrankie@gmail.com>
2020-11-16 13:32:54 +0800

    update setup.py and remove src folder

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='ColorStr',  
-     version='1.1',
-     scripts=['ColorStr'] ,
+     version='1.2',
      author="mclt0568",
      author_email="tis7bfrankie@gmail.com",
      description="A better way to output colored text",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/mclt0568/ColorStr",
-     packages=setuptools.find_packages(),
+     py_modules=["ColorStr"],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: Apache Software License",
