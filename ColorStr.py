c8f881da4aab15e96bd04ae4e2bf2e2c4b0abdcd
mclt0568 <tis7bfrankie@gmail.com>
2020-11-16 13:44:46 +0800

    arrainged the structure & renamed files to avoid confliction

--- ColorStr.py
+++ ColorStr.py
@@ -1,10 +1,10 @@
-import parser,sys
+import colorstrparser,sys
 
 def parse(value):
-	return parser.parse(value)
+	return colorstrparser.parse(value)
 
 if __name__ == "__main__":
 	if sys.argv[1:]:
-		print(parser.parse(sys.argv[1]))
+		print(colorstrparser.parse(sys.argv[1]))
 	else:
 		print("Please provide string as argument.")
