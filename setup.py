299015a56cada4d3f8c8a10e385923bc755ad79d
mclt0568 <tis7bfrankie@gmail.com>
2020-11-19 13:36:30 +0800

    added Restructure Text for pypi (markdown dont seems to work)

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
-with open("README.md", "r") as fh:
+with open("README", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
      name='ColorStr',  
-     version='1.2',
+     version='1.3',
      author="mclt0568",
      author_email="tis7bfrankie@gmail.com",
      description="A better way to output colored text",
      long_description=long_description,
-     long_description_content_type="text/markdown",
+     # long_description_content_type="text/markdown",
      url="https://github.com/mclt0568/ColorStr",
-     py_modules=["ColorStr"],
+     py_modules=["ColorStr","colorstrparser","colors"],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: Apache Software License",
